0cab8c9041d4539efcae34d0bf752850189c8204
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-04-01 19:02:33 -0500

    button prompts install, and disappears after install

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ installButton.style.fontWeight = 'bold';
 installButton.style.backgroundColor = 'blue';
 installButton.style.color = 'white';
 installButton.style.padding = '10px 20px';
-installButton.style.color = 'white';
 installButton.style.border = 'none';
 installButton.style.borderRadius = '15px';
 installButton.style.margin = '10px';
@@ -25,11 +24,27 @@ window.addEventListener('beforeinstallprompt', function(e){
   prompt = e;
 });
 
-installButton.addEventListener('click', function(){
-   prompt.prompt();
+
+let installed = false;
+
+installButton.addEventListener('click', async function(){
+  prompt.prompt();
+  let result = await prompt.userChoice;
+  if (result && result.outcome === 'accepted') {
+    console.log('accepted');
+    installed = true;
+}
+console.log('app install  = ', installed );
 })
 
 // Append the install button to the body
 const pwaPrompt = document.getElementById('pwaPrompt')
 
 pwaPrompt.appendChild(installButton);
+
+
+
+window.addEventListener('appinstalled', async function(e) {
+    console.log(e);
+    installButton.style.display = "none";
+ });
